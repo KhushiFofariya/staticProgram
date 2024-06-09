0183abf2ae35a91b36b7582582f3e65faf04a3f5
KhushiFofariya <148344155+KhushiFofariya@users.noreply.github.com>
2024-06-09 22:27:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank b1 = new Bank();
+		Bank b2 = new Bank();
+		Bank b3 = new Bank();
+		
+		System.out.println("The total number of account is:"+Bank.getAccount());
+	}
+
+}
